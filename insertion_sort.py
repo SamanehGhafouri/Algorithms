42d3429f6cd528957cebcccf407fb1e252d7a298
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2019-07-05 23:17:08 -0400

    Defined stub for insertion sort and 2 failing test

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,1 +1,30 @@
-# Please code me
+
+import unittest
+
+
+def insertion_sort(items):
+    return None
+
+# writing the failing test. we need to pass nonempty input.
+
+
+class TestInsertionSort(unittest.TestCase):
+
+    def test_nonempty_input(self):
+        items = [3, 8, 1, 0, 9]
+        expected = [0, 1, 3, 8, 9]
+        actual = insertion_sort(items)
+        # assert is func that defines the unittest
+        self.assertEqual(expected, actual, "Not sorted!")
+
+    def test_empty_input(self):
+        items = []
+        expected = []
+        actual = insertion_sort(items)
+        self.assertEqual(expected, actual, "empty")
+
+
+
+
+
+
